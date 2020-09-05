f3fc3789b275a453e9fdd702dc9d7ddaa9f9cc9e
Jason Nall <jsonnull@gmail.com>
2020-09-05 14:55:54 -0700

    Update verdaccio config and publishing.

--- scripts/publish-local.sh
+++ scripts/publish-local.sh
@@ -1,2 +1,17 @@
-NPM_CONFIG_REGISTRY=http://localhost:4873 ./scripts/exec.sh npm unpublish --force
-NPM_CONFIG_REGISTRY=http://localhost:4873 ./scripts/exec.sh npm publish
+custom_registry_url=http://localhost:4873
+original_npm_registry_url=`npm get registry`
+original_yarn_registry_url=`yarn config get registry`
+
+# Set registry to local registry
+npm set registry "$custom_registry_url"
+yarn config set registry "$custom_registry_url"
+
+# Login to publish packages
+(cd && npx npm-auth-to-token@1.0.0 -u user -p password -e user@example.com -r "$custom_registry_url")
+
+# ./scripts/exec.sh unpublish --force
+./scripts/exec.sh npm publish
+
+# Restore the original NPM and Yarn registry URLs
+npm set registry "$original_npm_registry_url"
+yarn config set registry "$original_yarn_registry_url"
