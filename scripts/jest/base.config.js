4fbc50f9a1271563d65b5b3f8ad77e07429444e0
Jason Nall <jsonnull@gmail.com>
2020-09-04 22:46:44 -0700

    Use ts-jest.

--- scripts/jest/base.config.js
+++ scripts/jest/base.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  preset: 'ts-jest',
   snapshotSerializers: ['jest-snapshot-serializer-raw'],
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
   testMatch: ['**/__tests__/**/*.test.[jt]s?(x)'],
