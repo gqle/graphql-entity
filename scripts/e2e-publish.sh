833407a20769862d625aa549ba1bf580b1f32a80
Jason Nall <jsonnull@gmail.com>
2020-09-07 12:09:26 -0700

    Also test package installation.

--- scripts/e2e-publish.sh
+++ scripts/e2e-publish.sh
@@ -1,8 +1,11 @@
-# Exit the script on any command with non 0 return code
+# Exit the script on any command with non-0 return code
 set -e
 
 # Echo every command being executed
 set -x
+
+# Start in root directory
+cd "$(dirname "$0")/.."
 
 custom_registry_url=http://localhost:4873
 original_npm_registry_url=`npm get registry`
@@ -12,10 +15,16 @@ original_yarn_registry_url=`yarn config get registry`
 npm set registry "$custom_registry_url"
 yarn config set registry "$custom_registry_url"
 
-# Login to publish packages
+# Test that installation works without pulling any monorepo packages off of npm
+# In other words, all monorepo package references should be the version in the repo
+yarn install --pure-lockfile
+
+# Build
+yarn run build
+
+# Login and publish packages locally
 (cd && npx npm-auth-to-token@1.0.0 -u user -p password -e user@example.com -r "$custom_registry_url")
 
-# ./scripts/exec.sh unpublish --force
 ./scripts/exec.sh npm publish
 
 # Restore the original NPM and Yarn registry URLs
